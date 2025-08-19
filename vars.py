6a9c0067f08357b8a79511774291a787b0ede02a
flani7761 <falani7761@gmail.com>
2025-08-19 13:58:21 +0530

    Add files via upload

--- vars.py
+++ vars.py
@@ -3,9 +3,21 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "28585138"))
-API_HASH = environ.get("API_HASH", "74b7afde7d9a546db3451cf4689f4e14")
+API_ID = int(environ.get("API_ID", "29490954"))
+API_HASH = environ.get("API_HASH", "dbd8f5af56b0f6e16327c20a84eece99")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "7181075926"))
+
+OWNER = int(environ.get("OWNER", "7660860610"))
+CREDIT = environ.get("CREDIT", "@RIYA40X")
+
+TOTAL_USER = os.environ.get('TOTAL_USERS', '5680454765').split(',')
+TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
+
+AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
+AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
+if int(OWNER) not in AUTH_USERS:
+    AUTH_USERS.append(int(OWNER))
+  
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
